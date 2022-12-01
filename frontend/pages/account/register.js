c0c3881c0eb56a3e5b6c3e8669881603c638dad6
kcansari <kcansari0@gmail.com>
2022-12-01 17:59:37 +0300

    not finished yet

--- frontend/pages/account/register.js
+++ frontend/pages/account/register.js
@@ -3,8 +3,6 @@ import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
 import CssBaseline from '@mui/material/CssBaseline'
 import TextField from '@mui/material/TextField'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Checkbox from '@mui/material/Checkbox'
 import Link from 'next/link'
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
@@ -13,17 +11,29 @@ import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import Layout from '@/components/Layout.js'
+import { useState, useEffect, useContext } from 'react'
+import Alert from '@mui/material/Alert'
+import Stack from '@mui/material/Stack'
 
 const theme = createTheme()
 
 export default function SignUp() {
+  const [firstname, setFirstName] = useState('')
+  const [lastname, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [errors, setError] = useState([])
+
+  console.log(errors)
+
   const handleSubmit = (event) => {
     event.preventDefault()
-    const data = new FormData(event.currentTarget)
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    })
+    if (password !== passwordConfirm) {
+      return setError((errors) => [...errors, 'Passwords do not match!'])
+    }
+
+    setError([])
   }
 
   return (
@@ -61,6 +71,8 @@ export default function SignUp() {
                     id='firstName'
                     label='First Name'
                     autoFocus
+                    value={firstname}
+                    onChange={(e) => setFirstName(e.target.value)}
                   />
                 </Grid>
                 <Grid item xs={12} sm={6}>
@@ -71,16 +83,21 @@ export default function SignUp() {
                     label='Last Name'
                     name='lastName'
                     autoComplete='family-name'
+                    value={lastname}
+                    onChange={(e) => setLastName(e.target.value)}
                   />
                 </Grid>
                 <Grid item xs={12}>
                   <TextField
                     required
                     fullWidth
+                    type={'email'}
                     id='email'
                     label='Email Address'
                     name='email'
                     autoComplete='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                   />
                 </Grid>
                 <Grid item xs={12}>
@@ -92,17 +109,24 @@ export default function SignUp() {
                     type='password'
                     id='password'
                     autoComplete='new-password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                   />
                 </Grid>
                 <Grid item xs={12}>
-                  <FormControlLabel
-                    control={
-                      <Checkbox value='allowExtraEmails' color='primary' />
-                    }
-                    label='I want to receive inspiration, marketing promotions and updates via email.'
+                  <TextField
+                    required
+                    fullWidth
+                    name='confirm password'
+                    label='Confirm Password'
+                    type='password'
+                    id='passwordConfirm'
+                    value={passwordConfirm}
+                    onChange={(e) => setPasswordConfirm(e.target.value)}
                   />
                 </Grid>
               </Grid>
+              {/* {errors.length !== 0 && <BasicAlerts error={errors} />} */}
               <Button
                 type='submit'
                 fullWidth
@@ -113,34 +137,19 @@ export default function SignUp() {
               </Button>
               <Grid container justifyContent='flex-end'>
                 <Grid item>
-                  <Link href='#' variant='body2'>
-                    Already have an account? Sign in
+                  <Link
+                    href='/account/login'
+                    style={{ textDecoration: 'none' }}
+                  >
+                    Already have an account? <strong>Sign in</strong>
                   </Link>
                 </Grid>
               </Grid>
             </Box>
           </Box>
-          <Copyright sx={{ mt: 5 }} />
+          {/* <Copyright sx={{ mt: 5 }} /> */}
         </Container>
       </ThemeProvider>
     </Layout>
-  )
-}
-
-function Copyright(props) {
-  return (
-    <Typography
-      variant='body2'
-      color='text.secondary'
-      align='center'
-      {...props}
-    >
-      {'Copyright © '}
-      <Link style={{ textDecoration: 'none', color: 'inherit' }} href='/'>
-        Vocabulary Hour
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
   )
 }
