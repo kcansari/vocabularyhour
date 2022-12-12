5b7b90dfc2fab6103d91ec0d7b9b559cbec032e3
kcansari <kcansari0@gmail.com>
2022-12-12 18:52:50 +0300

    Add validations, username, email and password

--- frontend/pages/account/register.js
+++ frontend/pages/account/register.js
@@ -1,39 +1,71 @@
 import * as React from 'react'
-import Avatar from '@mui/material/Avatar'
-import Button from '@mui/material/Button'
+import Visibility from '@mui/icons-material/Visibility'
+import VisibilityOff from '@mui/icons-material/VisibilityOff'
+import InputAdornment from '@mui/material/InputAdornment'
+import IconButton from '@mui/material/IconButton'
+import LoadingButton from '@mui/lab/LoadingButton'
 import CssBaseline from '@mui/material/CssBaseline'
 import TextField from '@mui/material/TextField'
+import Box from '@mui/material/Box'
 import Link from 'next/link'
 import Grid from '@mui/material/Grid'
-import Box from '@mui/material/Box'
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { styles } from '@/config/registerPageConfig.js'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import Avatar from '@mui/material/Avatar'
 import Layout from '@/components/Layout.js'
-import { useState, useEffect, useContext } from 'react'
-import Alert from '@mui/material/Alert'
-import Stack from '@mui/material/Stack'
+import * as yup from 'yup'
 
 const theme = createTheme()
 
-export default function SignUp() {
-  const [firstname, setFirstName] = useState('')
-  const [lastname, setLastName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordConfirm, setPasswordConfirm] = useState('')
-  const [errors, setError] = useState([])
+const emailRegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
 
-  console.log(errors)
+const validationSchema = yup
+  .object({
+    username: yup
+      .string()
+      .required('Username is required')
+      .min(3, 'Username must be at least 3 characters'),
+    email: yup
+      .string()
+      .required('Email is required')
+      .matches(emailRegExp, 'Email is invalid.'),
+    password: yup
+      .string()
+      .required('Password is required')
+      .min(6, 'Password must be at least 6 characters'),
+    confirmPassword: yup
+      .string()
+      .required('Confirm Password is required')
+      .oneOf([yup.ref('password')], 'Passwords must match'),
+  })
+  .required()
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    if (password !== passwordConfirm) {
-      return setError((errors) => [...errors, 'Passwords do not match!'])
-    }
+export default function SignIn() {
+  const [passwordVisibility, setPasswordVisibility] = React.useState(false)
 
-    setError([])
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  })
+
+  const onSubmit = (data) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve()
+      }, 2000)
+    })
+  }
+
+  const handleClickShowPassword = () => {
+    setPasswordVisibility(!passwordVisibility)
   }
 
   return (
@@ -41,113 +73,101 @@ export default function SignUp() {
       <ThemeProvider theme={theme}>
         <Container component='main' maxWidth='xs'>
           <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
+          <Box sx={styles.box}>
             <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
               <LockOutlinedIcon />
             </Avatar>
             <Typography component='h1' variant='h5'>
-              Sign up
+              Sign Up
             </Typography>
-            <Box
-              component='form'
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 3 }}
-            >
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    autoComplete='given-name'
-                    name='firstName'
-                    required
-                    fullWidth
-                    id='firstName'
-                    label='First Name'
-                    autoFocus
-                    value={firstname}
-                    onChange={(e) => setFirstName(e.target.value)}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id='lastName'
-                    label='Last Name'
-                    name='lastName'
-                    autoComplete='family-name'
-                    value={lastname}
-                    onChange={(e) => setLastName(e.target.value)}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    type={'email'}
-                    id='email'
-                    label='Email Address'
-                    name='email'
-                    autoComplete='email'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name='password'
-                    label='Password'
-                    type='password'
-                    id='password'
-                    autoComplete='new-password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name='confirm password'
-                    label='Confirm Password'
-                    type='password'
-                    id='passwordConfirm'
-                    value={passwordConfirm}
-                    onChange={(e) => setPasswordConfirm(e.target.value)}
-                  />
+            <Box component='form' onSubmit={handleSubmit(onSubmit)} noValidate>
+              <TextField
+                margin='normal'
+                required
+                fullWidth
+                id='username'
+                label='Username'
+                name='username'
+                autoComplete='email'
+                {...register('username')}
+                error={errors.username ? true : false}
+                helperText={errors.username?.message}
+              />
+              <TextField
+                margin='normal'
+                required
+                fullWidth
+                id='email'
+                label='Email Address'
+                name='email'
+                autoComplete='email'
+                {...register('email')}
+                error={errors.email ? true : false}
+                helperText={errors.email?.message}
+              />
+              <TextField
+                margin='normal'
+                required
+                fullWidth
+                id='password'
+                label='Password'
+                name='password'
+                type={passwordVisibility ? 'text' : 'password'}
+                autoComplete='password'
+                {...register('password')}
+                error={errors.password ? true : false}
+                helperText={errors.password?.message}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position='end'>
+                      {' '}
+                      <IconButton onClick={handleClickShowPassword} edge='end'>
+                        {passwordVisibility ? (
+                          <VisibilityOff />
+                        ) : (
+                          <Visibility />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <TextField
+                margin='normal'
+                required
+                fullWidth
+                id='confirmPassword'
+                label='Confirm Password'
+                name='confirmPassword'
+                type={passwordVisibility ? 'text' : 'password'}
+                autoComplete='confirmPassword'
+                {...register('confirmPassword')}
+                error={errors.confirmPassword ? true : false}
+                helperText={errors.confirmPassword?.message}
+              />
+              <Grid container justifyContent='flex-end' sx={{ marginTop: 2 }}>
+                <Grid item>
+                  Already have an account?
+                  <Link
+                    href='/account/login'
+                    variant='body2'
+                    style={{ textDecoration: 'none' }}
+                  >
+                    <strong> Sign in</strong>
+                  </Link>
                 </Grid>
               </Grid>
-              {/* {errors.length !== 0 && <BasicAlerts error={errors} />} */}
-              <Button
+              <LoadingButton
                 type='submit'
                 fullWidth
+                loading={isSubmitting}
                 variant='contained'
                 sx={{ mt: 3, mb: 2 }}
               >
                 Sign Up
-              </Button>
-              <Grid container justifyContent='flex-end'>
-                <Grid item>
-                  <Link
-                    href='/account/login'
-                    style={{ textDecoration: 'none' }}
-                  >
-                    Already have an account? <strong>Sign in</strong>
-                  </Link>
-                </Grid>
-              </Grid>
+              </LoadingButton>
             </Box>
           </Box>
-          {/* <Copyright sx={{ mt: 5 }} /> */}
         </Container>
       </ThemeProvider>
     </Layout>
