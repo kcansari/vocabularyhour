19096e5b44f6857d750530a28a29eb67dfff8775
kcansari <kcansari0@gmail.com>
2022-12-13 20:07:02 +0300

    deleted not necessary

--- frontend/pages/api/users/index.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    res.status(200).json({ name: 'John Doe' })
-  } else {
-    res.setHeader('Allow', ['POST'])
-    res.status(405).json({ message: `Method ${req.method} not allowed` })
-  }
-}
