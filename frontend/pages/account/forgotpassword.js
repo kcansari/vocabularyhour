7e1c48d6c19fdd5a13649cabca7a859ae80501c6
kcansari <kcansari0@gmail.com>
2022-12-19 16:46:39 +0300

    add close message button

--- frontend/pages/account/forgotpassword.js
+++ frontend/pages/account/forgotpassword.js
@@ -9,6 +9,10 @@ import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 import Stack from '@mui/material/Stack'
 import Alert from '@mui/material/Alert'
+import IconButton from '@mui/material/IconButton'
+import Collapse from '@mui/material/Collapse'
+
+import CloseIcon from '@mui/icons-material/Close'
 
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useForm } from 'react-hook-form'
@@ -27,6 +31,7 @@ const validationSchema = yup
 
 export default function Forgotpassword() {
   const [showMessage, setMessage] = useState(false)
+  const [open, setOpen] = useState(true)
 
   const {
     register,
@@ -39,13 +44,14 @@ export default function Forgotpassword() {
   const onSubmit = (data) => {
     if (data.email) {
       setMessage(true)
+      setOpen(true)
       console.log(data.email)
     }
   }
 
   return (
     <Layout title={'Forgot password'}>
-      <Container>
+      <Container maxWidth='xs'>
         <Box
           sx={{
             marginTop: 8,
@@ -71,7 +77,7 @@ export default function Forgotpassword() {
               <Divider
                 sx={{
                   borderColor: 'primary.main',
-                  border: 2,
+                  border: 1,
                   borderRadius: 1,
                   width: 1 / 2,
                 }}
@@ -96,13 +102,29 @@ export default function Forgotpassword() {
                   type='submit'
                   onClick={handleSubmit(onSubmit)}
                 >
-                  Submit
+                  Send
                 </Button>
               </Stack>
               {showMessage ? (
-                <Alert variant='outlined' severity='success'>
-                  Check your mail box
-                </Alert>
+                <Collapse in={open}>
+                  <Alert
+                    action={
+                      <IconButton
+                        aria-label='close'
+                        color='inherit'
+                        size='small'
+                        onClick={() => {
+                          setOpen(false)
+                        }}
+                      >
+                        <CloseIcon fontSize='inherit' />
+                      </IconButton>
+                    }
+                    sx={{ mb: 2 }}
+                  >
+                    We've sent your password reset link
+                  </Alert>
+                </Collapse>
               ) : (
                 ''
               )}
