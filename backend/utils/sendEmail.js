9e51ae74db8051c02c1324d61a3b58b0bea8a00b
kcansari <kcansari0@gmail.com>
2022-12-22 19:03:31 +0300

    add nodemalier boilerplate and basic html template

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,32 @@
+import nodemailer from 'nodemailer'
+
+const sendEmail = async (email, subject, text) => {
+  try {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: process.env.USER, // generated ethereal user
+        pass: process.env.PASS, // generated ethereal password
+      },
+    })
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Vocabulary Hour 👻" <vocabulary@hour>', // sender address
+      to: email, // list of receivers
+      subject: subject, // Subject line
+      text: text, // plain text body
+      html:
+        "To verify your account, click this <a href='" +
+        text +
+        "'> <b>link</b></a>", // html body
+    })
+  } catch (error) {
+    return error
+  }
+}
+
+export default sendEmail
