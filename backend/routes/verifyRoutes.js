58438dd25efd95828d1df93ea7458825f49ed6a0
kcansari <kcansari0@gmail.com>
2023-01-02 20:27:54 +0300

    add '/forgotpassword/:email route

--- backend/routes/verifyRoutes.js
+++ backend/routes/verifyRoutes.js
@@ -3,10 +3,11 @@ import { protect } from '../middleware/authMiddleware.js'
 import {
   verifyUser,
   resendVerifyLink,
+  sendForgotPasswordLink,
 } from '../controllers/verifyController.js'
 
 const router = express.Router()
-
+router.route('/forgotpassword/:email').get(sendForgotPasswordLink)
 router.route('/resend/:id').get(protect, resendVerifyLink)
 router.route('/:id/:token').get(verifyUser)
 
