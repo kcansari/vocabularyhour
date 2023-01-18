36b553f656b471634db42e40d0b5ddb13e5ca414
kcansari <kcansari0@gmail.com>
2023-01-18 18:31:55 +0300

    Create wordCollectionSchema

---
+++ backend/models/wordCollectionModel.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+const { Schema } = mongoose
+
+const wordCollectionSchema = new mongoose.Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: 'User' },
+    words: [{ type: Schema.Types.ObjectId, ref: 'Word' }],
+  },
+  {
+    // If you set timestamps: true, Mongoose will add two properties of type Date to your schema:
+    // createdAt: a date representing when this document was created
+    // updatedAt: a date representing when this document was last updated
+    timestamps: true,
+  }
+)
+
+const WordCollection = mongoose.model('WordCollection', wordCollectionSchema)
+
+export default WordCollection
