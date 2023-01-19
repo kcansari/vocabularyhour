21b9c86b2a23f6079d4c21d6e74481556aa1b3a5
kcansari <kcansari0@gmail.com>
2023-01-19 22:13:00 +0300

    add worcollection path

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import wordCollectionRoutes from './routes/wordCollectionRoutes.js'
 import wordRoutes from './routes/wordRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import verifyRoutes from './routes/verifyRoutes.js'
@@ -25,6 +26,7 @@ app.get('/', (req, res) => {
 app.use('/api/words', wordRoutes)
 app.use('/api/users', userRoutes)
 app.use('/verify', verifyRoutes)
+app.use('/wordcollection', wordCollectionRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
