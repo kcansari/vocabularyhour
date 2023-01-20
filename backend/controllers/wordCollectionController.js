30493d76c83b9cc319b3dce8913f3ebe0e6875be
kcansari <kcansari0@gmail.com>
2023-01-20 21:19:34 +0300

    add put word, delete collection, update word methods

--- backend/controllers/wordCollectionController.js
+++ backend/controllers/wordCollectionController.js
@@ -16,45 +16,72 @@ const getCollections = asyncHandler(async (req, res) => {
     })
 })
 
-// @desc Create a word collection
+// @desc Fetch specific user's collections
+// @route GET /wordcollection/user
+// @access Private
+const getUserCollections = asyncHandler(async (req, res) => {
+  WordCollection.find({ userId: req.user._id })
+    .populate('userId', 'username') // a few specific fields returned for the populated documents
+    .populate('words')
+    .exec(function (err, wordcollection) {
+      if (err) return handleError(err)
+      res.json(wordcollection)
+    })
+})
+
+// @desc Create a new word collection
 // @route POST /wordcollection/create
 // @access Private
 const createCollection = asyncHandler(async (req, res) => {
-  const { collectionName, wordName, meaning } = req.body
+  const { collectionName } = req.body
+
+  const newCollection = new WordCollection({
+    name: collectionName,
+    userId: req.user._id,
+  })
+  const newCollectionSaved = await newCollection.save()
+  res.json(newCollectionSaved)
+})
+
+// @desc Add a new word to an exist collection
+// @route PUT /wordcollection/add/:collectionId
+// @access Private
+const addNewWordToCollection = asyncHandler(async (req, res) => {
+  const { collectionId } = req.params
+  const { wordName, meaning } = req.body
   const existWord = await Word.findOne({ name: wordName })
+  const collection = await WordCollection.find({ _id: collectionId })
+  const { words } = collection[0]
+  let word
 
-  if (!wordName && !meaning) {
-    res.status(404)
-    throw new Error('Write a valid word')
-  }
-
-  if (existWord) {
-    const newCollection = new WordCollection({
-      name: collectionName,
-      userId: req.user._id,
-      words: [existWord._id],
-    })
-
-    const newCollectionSaved = await newCollection.save()
-
-    res.json(newCollectionSaved)
-  } else {
-    const word = new Word({
+  if (!existWord) {
+    word = new Word({
       name: wordName,
       meaning: meaning,
     })
     await word.save()
-
-    const newCollection = new WordCollection({
-      name: collectionName,
-      userId: req.user._id,
-      words: word._id,
-    })
-
-    await newCollection.save()
-
-    res.json(newCollection)
   }
+  const updatedCollection = await WordCollection.findByIdAndUpdate(
+    collectionId,
+    { words: [...words, existWord ? existWord._id : word._id] },
+    { new: true }
+  )
+  res.json(updatedCollection)
 })
 
-export { getCollections, createCollection }
+// @desc Delete a specific word collection
+// @route Delete /wordcollection/delete/collection/:collectionId
+// @access Private
+const deleteCollection = asyncHandler(async (req, res) => {
+  const { collectionId } = req.params
+  let collection = await WordCollection.findByIdAndDelete(collectionId)
+  res.json({ message: 'Collection was deleted', data: collection })
+})
+
+export {
+  getCollections,
+  createCollection,
+  getUserCollections,
+  addNewWordToCollection,
+  deleteCollection,
+}
--- backend/routes/wordCollectionRoutes.js
+++ backend/routes/wordCollectionRoutes.js
@@ -2,6 +2,9 @@ import express from 'express'
 import {
   getCollections,
   createCollection,
+  getUserCollections,
+  addNewWordToCollection,
+  deleteCollection,
 } from '../controllers/wordCollectionController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -9,5 +12,10 @@ const router = express.Router()
 
 router.route('/').get(protect, admin, getCollections)
 router.route('/create').post(protect, createCollection)
+router.route('/user').get(protect, getUserCollections)
+router.route('/add/:collectionId').put(protect, addNewWordToCollection)
+router
+  .route('/delete/collection/:collectionId')
+  .delete(protect, deleteCollection)
 
 export default router
