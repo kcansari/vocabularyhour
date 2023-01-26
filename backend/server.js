233683fbc9a2fcf5d89ad23b06717e51c337c8b9
kcansari <kcansari0@gmail.com>
2023-01-26 18:41:53 +0300

    removed wordCollectionRotues function

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import wordCollectionRoutes from './routes/wordCollectionRoutes.js'
 import wordRoutes from './routes/wordRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import verifyRoutes from './routes/verifyRoutes.js'
@@ -26,7 +25,6 @@ app.get('/', (req, res) => {
 app.use('/api/words', wordRoutes)
 app.use('/api/users', userRoutes)
 app.use('/verify', verifyRoutes)
-app.use('/wordcollection', wordCollectionRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
