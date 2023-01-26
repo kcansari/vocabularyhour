cd8ad0f5d88249de5c490574e4e4e9548277d284
kcansari <kcansari0@gmail.com>
2023-01-26 21:46:07 +0300

    add sample words to initialize

--- backend/data/users.js
+++ backend/data/users.js
@@ -7,12 +7,22 @@ const users = [
     password: bcrypt.hashSync('123456', 10),
     isAdmin: true,
     verified: true,
+    Words: {
+      bottle: 'şişe',
+      keyboard: 'klavye',
+      metaphor: 'mecaz',
+    },
   },
   {
     username: 'Kemal Can',
     email: 'kemalcan@example.com',
     password: bcrypt.hashSync('123456', 10),
     verified: true,
+    Words: {
+      'residence permit': 'oturma izni',
+      migrate: 'göç etmek',
+      proverb: 'atasözü',
+    },
   },
 ]
 
